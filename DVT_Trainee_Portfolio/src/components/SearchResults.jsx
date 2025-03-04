8a3894bba2f91b4b51de9c601bc39ca2adc69c14
Thabane Ngobe <tngobe@dvtsoftware.com>
2025-03-05 00:07:21 +0200

    created a new user card

--- DVT_Trainee_Portfolio/src/components/SearchResults.jsx
+++ DVT_Trainee_Portfolio/src/components/SearchResults.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom"
 import SearchNav from "./SearchNav";
 import Avatar from '@mui/material/Avatar';
 import './SearchResults.css';
+import UserCard from "./UserCard";
+import Pagination from '@mui/material/Pagination';
 
 export default function SearchResults({results, resultsCopy, filter}) {
   
@@ -20,55 +22,66 @@ export function ResultsList({results, id}){
   if(results.length == 0){
     return <h1>Results not Found</h1>
   }
-  
+
+  const employees = results.filter((result) => {return result.project_id === undefined});
+  const projects = results.filter((result) =>{return result.project_id !== undefined});
+
   return (
-    <section className="results-list">
-      {
-        results.map((result, index) => {
-          return ( 
-            <Results result={result} isProject={result.project_id !== undefined} />
-          );})
+    <>
+      <div className="users-list">
+        {
+          employees.map((user, indes) =>{
+            return (<UserCard user={user}  key={indes}/>)
+          })
         }
-    </section>
+      </div>
+      <section className="results-list">
+        {
+          projects.map((result, index) => {
+            return ( 
+              <Results result={result}/>
+            );})
+          }
+        <Pagination count={results.length} variant="outlined" color="primary"  className="pagination"/>
+      </section>
+    </>
   )
 }
 
-export function Results({result, isProject}) {
+export function Results({result}) {
 
-  if(isProject){
-    return (
-      <div className="results">
-       <div className="card-text">
-          <div className="intro">
-            <p className="results-title">{result.title}</p>
-            <p className="results-type">{result.created_on}</p>
-          </div>
-  
-          <div className="results-data">
-            <div className="results-p">
-              <p className="results-bio text-container">{result.description}</p>
-            </div>
-          </div>
-          <div className="user-skills">
-            {
-              <GenerateBadges badgeList={result.technologies} />
-            }
-          </div>
-          <div className="card-footer">
-            <div className="user-profile">
-              <div className="users-pic">
-                <Avatar src="/Gomo.jpg" />
-              </div>
-              <Link className="user-name">@Thabane</Link>
-            </div>
-            <Link to={'/userportfolio'}>
-              <button className="port-btn">View project</button>
-            </Link>
+  return (
+    <div className="results">
+      <div className="card-text">
+        <div className="intro">
+          <p className="results-title">{result.title}</p>
+          <p className="results-type">{result.created_on}</p>
+        </div>
+
+        <div className="results-data">
+          <div className="results-p">
+            <p className="results-bio text-container">{result.description}</p>
           </div>
         </div>
-        </div> 
-    );
-  }
+        <div className="user-skills">
+          {
+            <GenerateBadges badgeList={result.technologies} />
+          }
+        </div>
+        <div className="card-footer">
+          <div className="user-profile">
+            <div className="users-pic">
+              <Avatar src="/Gomo.jpg" />
+            </div>
+            <Link className="user-name">@Thabane</Link>
+          </div>
+          <Link to={'/userportfolio'}>
+            <button className="port-btn">View project</button>
+          </Link>
+        </div>
+      </div>
+      </div> 
+  );
 }
 
 
