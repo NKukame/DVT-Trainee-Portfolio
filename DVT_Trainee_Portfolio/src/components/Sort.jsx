6d3b36614d49aa87a3dbbdeb12ee628c1dc7c5fd
Thabane-Ngobe <tngobe@dvt.co.za>
2025-03-26 07:32:09 +0200

    added back the sort

--- DVT_Trainee_Portfolio/src/components/Sort.jsx
+++ DVT_Trainee_Portfolio/src/components/Sort.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react';  // Import only necessary React hooks
 import './Sort.css';  // Import the CSS file
+import SwapVertIcon from '@mui/icons-material/SwapVert';
 
 const SortDropdown = () => {
-  const [selectedOption, setSelectedOption] = useState('');
-
-  const handleChange = (event) => {
-    setSelectedOption(event.target.value);
-    // Handle sorting logic here based on the selected option
-    console.log(`Selected sort option: ${event.target.value}`);
-  };
-
+  
   return (
-    <div className="dropdown-container">
-      <label htmlFor="sort-options" className="dropdown-label">
-        {/* Sort by */}
-      </label>
-      <select
-        id="sort-options"
-        className="dropdown"
-        value={selectedOption}
-        onChange={handleChange}
-      >
-        {/* <option value="">Select Sort Option</option> */}
-        <option value="alphabetical-asc">Alphabetically, A-Z</option>
-        <option value="alphabetical-desc">Alphabetically, Z-A</option>
-        <option value="date-old-new"> old to new</option>
-        <option value="date-new-old"> new to old</option>
-      </select>
-    </div>
+    <button className="result-nav-btn sort-btn" onClick={''}>
+          <span className="sort-txt">Sort</span>
+          <SwapVertIcon/>
+    </button>
   );
 };
 
