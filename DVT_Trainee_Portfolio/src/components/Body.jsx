775da7319bc3bb357615afcd66919630e1e530f7
NKukame <NKukame@jhb.dvt.co.za>
2025-04-03 14:25:43 +0200

    Refactor styles and layout for About, Portfolio, and GridView components; enhance Search component structure

--- DVT_Trainee_Portfolio/src/components/Body.jsx
+++ DVT_Trainee_Portfolio/src/components/Body.jsx
@@ -3,14 +3,14 @@ import profileIcon from "../assets/placeholder.png";
 import React, { useState, useEffect } from "react";
 
 function Body() {
-    const [team, setTeam] = useState([]);
+  const [team, setTeam] = useState([]);
 
-    useEffect(() => {
-        fetch("/team-portfolio.json") 
-            .then((response) => response.json())
-            .then((data) => setTeam(data))
-            .catch((error) => console.error("Error loading team data:", error));
-    }, []);
+  useEffect(() => {
+    fetch("/team-portfolio.json")
+      .then((response) => response.json())
+      .then((data) => setTeam(data))
+      .catch((error) => console.error("Error loading team data:", error));
+  }, []);
 
   return (
     <div className="container">
@@ -20,27 +20,27 @@ function Body() {
           Welcome our class of 2025 and get ready to be blown away! This group
           of fresh talent is expertly trained and ready to invigorate your team
         </h2>
-        </div>
+      </div>
 
-        <div className="home-carousel-wrapper">
-
-
-            <div className="home-carousel">
-
-                {team.concat(team).map((person, index) => (
-                    <div key={index} className="home-carousel-item">
-                        <img src={person.image} alt={person.name} className="home-carousel-item-img" />
-                        <div className="home-carousel-item-text">
-                            <h3>{person.name}</h3>
-                            <p>{person.description}</p>
-                            <p>{person.techStack}</p>
-                        </div>
-                    </div>
-                ))}
-                
+      <div className="home-carousel-wrapper">
+        <div className="home-carousel">
+          {team.concat(team).map((person, index) => (
+            <div key={index} className="home-carousel-item">
+              <img
+                src={person.image}
+                alt={person.name}
+                className="home-carousel-item-img"
+              />
+              <div className="home-carousel-item-text">
+                <h3>{person.name}</h3>
+                <p>{person.description}</p>
+                <p>{person.techStack}</p>
+              </div>
             </div>
+          ))}
         </div>
-    <div className="home-content">
+      </div>
+      <div className="home-content">
         <div className="home-bottom">
           <p>
             Discover the bright minds shaping the future of technology at our
@@ -49,9 +49,7 @@ function Body() {
             team.
           </p>
           <button className="cta-button">The Team</button>
-          
         </div>
-
       </div>
     </div>
   );
