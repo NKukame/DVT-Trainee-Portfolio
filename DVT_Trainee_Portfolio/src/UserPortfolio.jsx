cfc8c0881092cc6e5f30fc67286e26284a90b281
Njabulo Kukame <NKukame@jhb.dvt.co.za>
2025-04-08 09:46:51 +0200

    Revert "Paballo's Changes Into "

--- DVT_Trainee_Portfolio/src/UserPortfolio.jsx
+++ DVT_Trainee_Portfolio/src/UserPortfolio.jsx
@@ -5,35 +5,40 @@ import ProjectCard from './ProjectCard';
 import Header from './components/Header';
 
 function UserPortfolio() {
+    // const projects = [0, 0, 0, 0];
     return (
         <>
             <Header />
-            <div className="userprofile-body">
+
+            {/* About Section */}
+            <div className="about-section">
+
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </p>
+
+            </div>
+
+            <Dashboard />
+            
+            <div className="project-container">
+                 
+                {/* Main Content */}
 
 
-                <div className="about-section">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-                    </p>
-                </div>
-
-                {/* New flex container */}
-                <div className="portfolio-layout">
-                    <Dashboard />
-                    <div className="project-container">
-                        <main className="main-content">
-                            <section className="grid-content">
-                                {Array.from({ length: 6 }).map((_, index) => (
-                                    <ProjectCard
-                                        key={index}
-                                        title="Project Name"
-                                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-                                    />
-                                ))}
-                            </section>
-                        </main>
-                    </div>
-                </div>
+                <main className="main-content">
+  
+                {/* Grid Content */}
+                <section className="grid-content">
+                    {Array.from({ length: 6 }).map((_, index) => (
+                        <ProjectCard
+                            key={index}
+                            title="Project Name"
+                            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+                        />
+                    ))}
+                </section>
+                </main>
             </div>
         </>
     );
