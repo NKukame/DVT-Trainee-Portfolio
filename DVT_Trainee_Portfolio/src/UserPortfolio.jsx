e3b94a56e0162b18d2cfe3879b643a8bc21102b4
pthobei <pthobei@dvtsoftware.com>
2025-04-15 10:37:33 +0200

    Remove unnecessary comment in UserPortfolio component for cleaner code

--- DVT_Trainee_Portfolio/src/UserPortfolio.jsx
+++ DVT_Trainee_Portfolio/src/UserPortfolio.jsx
@@ -24,7 +24,7 @@ function UserPortfolio() {
                         </p>
                     </div>
 
-                    {/* New flex container */}
+                    
                     <div className="portfolio-layout">
                         <Dashboard />
                         <div className="project-container">
