bd5465491975a98d7427be376d4dfc5ac1d20b96
Thabane Ngobe <tngobe@dvtsoftware.com>
2025-04-15 06:39:11 +0200

    Updateted the project structure for the search page

--- DVT_Trainee_Portfolio/src/components/Search/User/UserCard.jsx
+++ DVT_Trainee_Portfolio/src/components/Search/User/UserCard.jsx
@@ -6,7 +6,7 @@ export function UserCard({ user, showPortfolioLink = true, showDetails = true })
   return (
     <div className="card-user flex-col-start">
       <div className='flex-row-center'>
-        <Avatar alt={user.name} src={user.avatar || ''} sx={{ width: 50, height: 50 }} />
+        <Avatar alt={user.name} src={user.avatar || ''} sx={{ width: 52, height: 52 }} />
         <div className="flex-col-fill">
           <p className='text-lg-bold'>{user.name}</p>
           <p className='text-md-muted'>{user.role}</p>
