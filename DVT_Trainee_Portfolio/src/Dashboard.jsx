7414eef223a5528ed9f48be2e6a7351af8d05025
pthobei <pthobei@dvtsoftware.com>
2025-04-16 15:05:25 +0200

    Update Dashboard layout and styles; add icons for role, experience, availability, and location; enhance ProjectCard and TestimonialCard styles.

--- DVT_Trainee_Portfolio/src/Dashboard.jsx
+++ DVT_Trainee_Portfolio/src/Dashboard.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProfileImage from './assets/icons8-profile-picture-100.png';
+import Calender from './assets/icons8-calendar-100.png';
+import Location from './assets/icons8-location-100.png';
+import Medal from './assets/icons8-medal-100.png';
+import Tech from './assets/icons8-computer-100.png';
 import './Dashboard.css';
 
 function Dashboard() {
@@ -16,15 +20,26 @@ function Dashboard() {
                 
                 <div className="profile-info">
                     <h2>Full Name</h2>
-                    <p>Role</p>
-                    <p>Experience</p>
+                    <p><img src={Tech} alt="Tech placeholder" className="icon"/>Role</p>
+                    
+                    <p><img src={Medal} alt='Role Icon' className="icon"/>Experience</p>
+                    <p><img src={Calender} alt="Experience Icon" className="icon" /> Availability</p>
+                    <p><img src={Location} alt="Location Icon" className="icon" /> Location</p>
+
                     <Link to='/ProfileForm'><button className='manage-prfl'>Manage Profile</button></Link>
+
                 </div>
                 <div className="proficiencies">
                     <h2>Proficiencies</h2>
-                    <p><strong>Languages:</strong> List here</p>
-                    <p><strong>Frameworks:</strong> List here</p>
-                    <p><strong>OS:</strong> List here</p>
+                    <ul className='proficiency-list'>
+                        <li className='tag'>Java</li>
+                        <li className='tag'>Python</li>
+                        <li className='tag'>React</li>
+                        <li className='tag'>Node.js</li>
+                        <li className='tag'>Express.js</li>
+                        <li className='tag'>MongoDB</li>
+                        <li className='tag'>SQL</li>
+                    </ul>
                 </div>
                 <button className='cv-btn'>Download CV</button>
             </div> 
