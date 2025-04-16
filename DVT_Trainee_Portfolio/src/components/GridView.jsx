bab8c8a1c5fefcbe1263b9dcf104167fef1420bd
PhimiTm <PMakwatse@dvtsoftware>
2025-04-16 13:37:56 +0200

    feat: optimize rendering and improve layout in GridView component

--- DVT_Trainee_Portfolio/src/components/GridView.jsx
+++ DVT_Trainee_Portfolio/src/components/GridView.jsx
@@ -11,20 +11,22 @@ const GridView = ({ team }) => {
             <div
               className="grid-profile-pic"
               style={{ backgroundImage: `url(${member.image})` }}
-            ></div>
-
-            <div className="grid-portfolio-card-content">
-              <p className="grid-portfolio-card-title">{member.name}</p>
-              <p className="grid-portfolio-card-description">
-                {member.description}
-              </p>
-              <p className="grid-portfolio-card-description">
-                {member.techStack}
-              </p>
-              <Link to="/UserPortfolio">
-                <button className="grid-profile-card-button">Profile</button>
-              </Link>
+            >
+              
             </div>
+
+            <div className="grid-title-container">
+
+              <p className="grid-portfolio-card-title">{member.name}</p>
+
+              <div className="grid-portfolio-card-content">
+                <p className="grid-portfolio-card-description">{member.description}</p>
+                <p className="grid-portfolio-card-description">{member.techStack}</p>
+              </div>
+
+            </div>
+
+            
           </div>
         ))}
       </div>
