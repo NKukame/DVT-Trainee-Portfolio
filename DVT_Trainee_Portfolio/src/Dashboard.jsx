288f633cc2386418a6412e3bdcca28387596fea3
pthobei <pthobei@dvtsoftware.com>
2025-04-17 13:38:14 +0200

    Enhance Dashboard layout and styles; add social media icons, improve profile section, and adjust CSS for better responsiveness.

--- DVT_Trainee_Portfolio/src/Dashboard.jsx
+++ DVT_Trainee_Portfolio/src/Dashboard.jsx
@@ -4,29 +4,38 @@ import ProfileImage from './assets/icons8-profile-picture-100.png';
 import Calender from './assets/icons8-calendar-100.png';
 import Location from './assets/icons8-location-100.png';
 import Medal from './assets/icons8-medal-100.png';
+import Github from './assets/icons8-github-100.png';
+import LinkedIn from './assets/icons8-linkedin-100.png';
+import Email from './assets/icons8-email-100.png';
 import Tech from './assets/icons8-computer-100.png';
 import './Dashboard.css';
 
 function Dashboard() {
     return (
         <div className="dashboard">
-            
+
             <div className="profile">
 
-                
+
                 <div className='profile-picture'>
-                <img src={ProfileImage} className='profile-img' alt="Profile" />
+                    <img src={ProfileImage} className='profile-img' alt="Profile" />
                 </div>
-                
-                <div className="profile-info">
+
+                <div className="short-bio">
                     <h2>Full Name</h2>
-                    <p><img src={Tech} alt="Tech placeholder" className="icon"/>Role</p>
-                    
-                    <p><img src={Medal} alt='Role Icon' className="icon"/>Experience</p>
+                    <p>Role</p>
+                    <p>I like banana splits and things.</p>
+                </div>
+
+                <div className="profile-info">
+
+                    <Link to='/ProfileForm'><button className='manage-prfl'>Manage Profile</button></Link>
+
+                    <p><img src={Medal} alt='Role Icon' className="icon" />Experience</p>
                     <p><img src={Calender} alt="Experience Icon" className="icon" /> Availability</p>
                     <p><img src={Location} alt="Location Icon" className="icon" /> Location</p>
 
-                    <Link to='/ProfileForm'><button className='manage-prfl'>Manage Profile</button></Link>
+
 
                 </div>
                 <div className="proficiencies">
@@ -41,8 +50,13 @@ function Dashboard() {
                         <li className='tag'>SQL</li>
                     </ul>
                 </div>
-                <button className='cv-btn'>Download CV</button>
-            </div> 
+
+            </div>
+            <footer className='footer'>
+                <img src={Github} alt="GitHub" className="socials" />
+                <img src={LinkedIn} alt="LinkedIn" className="socials" />
+                <img src={Email} alt="Email" className="socials" />
+            </footer>
         </div>
     );
 }
