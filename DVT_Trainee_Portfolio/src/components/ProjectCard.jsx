508f5f3fe234154ef510730c038a294b9d1edcf4
Thabane-Ngobe <tngobe@dvt.co.za>
2025-05-14 15:11:42 +0200

    Updated the project modal card

--- DVT_Trainee_Portfolio/src/components/ProjectCard.jsx
+++ DVT_Trainee_Portfolio/src/components/ProjectCard.jsx
@@ -30,33 +30,39 @@ export default function ProjectCard({ result, showAuthor = false, showTech = tru
       {
        open && 
        <div className='modal-overlay'> 
-          <div className='modal-content flex-col project-modal-width gap-10-px rounded'>
+          <div className='modal-content flex-col gap-10-px project-modal-width gap-10-px rounded shadow'>
             <button className='modal-close' onClick={()=> setOpen(false)}>X</button>
 
-            <div className='flex flex-row flex-row-between'> 
-              <h1>{result.name}</h1>
+            <div className='flex-row align-items-center flex-row-between'> 
               <div>
-                <p>Author/s</p>
+                <h1 className='font-size-20-px'>{result.name}</h1>
+              </div>
+              <div className='flex-row gap-4-px'>
+                <div>
+                  <img src={result.avatar} alt="" className='profile-image' />
+                </div>
+                <p className='font-size-14-px'>{result.username}</p>
               </div>
             </div>
 
             <div>
               <img src={`./${result.screenshot}` }
-              className='modal-project-image'
+              className='modal-project-image rounded'
               alt="" />
             </div>
             <div>
+              <h2 className='m-10px'>Project Description</h2>
               <p>{result.description}</p>
             </div>
             <div className='h-1 w-full bg-gray'></div>
             <div>
-              <h2>Industries</h2>
+              <h2 className='m-10-px'>Industries</h2>
               <Badge badge={result.platform} />
             </div>
 
             <div className='h-1 w-full bg-gray'></div>
             <div>
-              <h2>Tech Stack</h2>
+              <h2 className='m-10-px'>Tech Stack</h2>
               <Badges badgeList={result.technologies} sliceList={false}/>
             </div>
             <div className='h-1 w-full bg-gray'></div>
