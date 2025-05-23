8ea1ff3e0dac01f9f611fa44d7aefd27cdaa060e
Remoneilwe Rammekwa <rrammekwa@jhb.dvt.co.za>
2025-05-23 12:57:46 +0200

    initial commit

---
+++ DVT_Trainee_Portfolio/frontend/src/api/index.ts
@@ -0,0 +1,15 @@
+
+// frontend/src/api/index.ts
+// import axios from 'axios';
+ 
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api', // match Express server base route
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+ 
+// export default api;
+
+
+// api
