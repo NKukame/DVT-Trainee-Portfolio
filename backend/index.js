8314f8ecfcd6d3a307ee1a1a752859abbbe7efaf
amhlongo <amhlongo@dvtsoftware.com>
2025-05-28 09:31:10 +0200

    feat: modified the prisma client to genrate in the node modules

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
 const app = express();
 const port = 3000;
-const { PrismaClient } = require('./generated/prisma')
 
 const prisma = new PrismaClient();
 
 app.get('/', async (req, res) => {
-  await prisma.$connect()
-  const users = await prisma.employee.findMany()
-  res.send(users)
+  await prisma.$connect();
+  const users = await prisma.user.findMany();
+  res.send(users);
 });
 
 app.listen(port, () => {
