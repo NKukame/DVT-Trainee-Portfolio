6f5b4da43ac53ca7d35fe62c0ca04bd95ce9bbf5
NKukame <NKukame@jhb.dvt.co.za>
2025-05-29 14:44:44 +0200

    Enhance Profile Creation UI: Add layout and styling for content and footer, integrate Stepper component, and improve button functionality

--- frontend/src/Pages/ProfileCreation/ProfileCreation.jsx
+++ frontend/src/Pages/ProfileCreation/ProfileCreation.jsx
@@ -2,6 +2,7 @@ import '../../styles.css'
 import './ProfileCreation.css';
 import SideBar from '../../components/SidebarComp/SideBar';
 import Stepper from '../../components/ProfileCreationComp/Stepper';
+import {Save, X} from 'lucide-react'
 import { useDarkMode } from '../../components/DarkModeComp/DarkModeProvider';
 
 function ProfileCreation(){
@@ -15,9 +16,37 @@ function ProfileCreation(){
                 <div className='app-layout-body'>
 
                     <div className="profile-creation-body">
-                        
+
                         <div className='profile-creation-header'>
-                            <Stepper/>
+                            <Stepper currentStep={0}/>
+                        </div>
+
+                        <div className='profile-creation-content'>
+
+                            <div className='profile-creation-content-header'>
+                                <h1>Body</h1>
+                            </div>
+
+                            <div className="profile-creation-content-forms-container">
+                                <h1>Forms</h1>
+                            </div>
+
+                            <div className="profile-creation-content-footer">
+                                <div className="profile-creation-content-footer-left-sided-buttons">
+                                    <button className='profile-creation-cancel-btn'> <X size={15} className='profile-creation-button-icon'/>Cancel</button>
+                                    <button className='profile-creation-save-btn'> <Save size={15} className='profile-creation-button-icon'/>Save & Exit</button>
+                                </div>
+
+                                <div className="profile-creation-content-footer-page-numbers">
+                                    <p>Page 1 of 7</p>
+                                </div>
+
+                                <div className="profile-creation-content-footer-left-sided-buttons">
+                                    <button>Previous</button>
+                                    <button>Next</button>
+                                </div>
+                            </div>
+
                         </div>
                         
                     </div>
