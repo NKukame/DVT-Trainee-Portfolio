99bb729bc5a30724f573835c38b4dabd72b73a37
Remoneilwe Rammekwa <rrammekwa@jhb.dvt.co.za>
2025-06-05 10:34:58 +0200

    jwt-middleware added

--- backend/routes/TotalRoutes.js
+++ backend/routes/TotalRoutes.js
@@ -7,25 +7,23 @@ import {UpdateProfileController} from '../controllers/UpdateProfileController.js
 import {forgotPassword} from '../controllers/ForgotPasswordController.js';
 import { deleteProjectController, deleteProfileController } from '../controllers/DeleteController.js';
 import signup from '../controllers/SignupController.js';
+import { authenticateToken } from '../middleware/authenticateToken.js';
 
 const totalRoutes = express.Router();
 
 totalRoutes.post('/login', login);
 totalRoutes.post('/register', signup);
-totalRoutes.post('/create-profile', createProfileController); //done
-
 totalRoutes.put('/forgot-password', forgotPassword);
-totalRoutes.put('/profile/:name', UpdateProfileController)
 
+totalRoutes.post('/create-profile', authenticateToken , createProfileController);
+totalRoutes.put('/profile/:name', authenticateToken , UpdateProfileController);
 
-totalRoutes.delete('/project/:id', deleteProjectController);
-totalRoutes.delete('/profile/:email', deleteProfileController);
+totalRoutes.delete('/project/:id', authenticateToken , deleteProjectController);
+totalRoutes.delete('/profile/:email', authenticateToken , deleteProfileController);
 
-
-totalRoutes.get('/project-showcase', HomeProjectController);
-totalRoutes.get('/team-cards-showcase', HomePortfolioController); // /profiles/:id
-
-totalRoutes.get('/profile/:name', SearchEmployeeController);
-totalRoutes.get('/project/:name', SearchProjectController);
+totalRoutes.get('/project-showcase', authenticateToken, HomeProjectController);
+totalRoutes.get('/team-cards-showcase', authenticateToken, HomePortfolioController); 
+totalRoutes.get('/profile/:name', authenticateToken, SearchEmployeeController);
+totalRoutes.get('/project/:name', authenticateToken, SearchProjectController);
 
 export default totalRoutes;
