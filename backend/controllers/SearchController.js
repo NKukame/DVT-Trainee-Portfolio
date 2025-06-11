93b2b23f3f101b013048f2110eaa0badfcc8fa48
amhlongo <amhlongo@dvtsoftware.com>
2025-06-11 20:16:55 +0200

    Refactor SearchController to implement project search functionality and add .gitignore file

--- backend/controllers/SearchController.js
+++ backend/controllers/SearchController.js
@@ -1,30 +1,69 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
- 
-export async function SearchEmployeeController(req, res)  {
-  const { name } = req.params;
 
-  const user = await prisma.user.findFirst({
-      where: { name },
-    });
-
-  if (!user) {
-    return res.status(404).send({ message: 'Employee not found' });
-  }
-
-  res.send(user);
-};
 
 export async function SearchProjectController(req, res) {
-  const { name } = req.params;
+  const {query, filters={}, sort, page = 1, limit = 10 } = req.params;
 
-  const selectedProject = await prisma.project.findFirst({
-      where: { name }
-  })
-  
-    if (!selectedProject) {
-    return res.status(404).send({ message: 'Project not found' });
+  const where = {}
+
+  if(query) {
+    where.OR = [
+      { name: { contains: query, mode: 'insensitive' } },
+      { description: { contains: query, mode: 'insensitive' } },
+    ]
   }
 
-  res.send(selectedProject);
-}
+  if (filters.industries?.length){
+    where.industry = { 
+      some: {
+        in: filters.industries 
+      }
+    }
+  }
+
+  if (filters.projectTechStack?.length){
+    where.techStack = { 
+      some: {
+        in: filters.projectTechStack 
+      }
+    }
+  }
+
+  const orderBy = {}
+
+  if (sort) {
+    orderBy[sort.field] = sort.order
+  }else {
+    orderBy.createdAt = 'desc'
+  }
+
+  const [projects, total] = await Promise.all([
+    prisma.project.findMany({
+      where,
+      orderBy,
+      skip: (page - 1) * limit,
+      take: limit,
+      include: {
+        industries: true,
+        techStack: true,
+        members: {
+          include: {
+            employee: true
+          }
+        }
+      }
+    }),
+    prisma.project.count({ where })
+  ]);
+
+  if (!projects) {
+    return res.status(404).send({ message: "Projects not found" });
+  }
+
+  return res.send({ projects, total });
+
+
+
+  }
+
