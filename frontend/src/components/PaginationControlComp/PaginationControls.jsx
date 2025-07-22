952aaf09c0ec26beb79fc078d0f12af2d007c458
Thabane Ngobe <tngobe@dtsoftware.com>
2025-07-22 17:49:59 +0200

    paginatioin version 1

--- frontend/src/components/PaginationControlComp/PaginationControls.jsx
+++ frontend/src/components/PaginationControlComp/PaginationControls.jsx
@@ -1,90 +1,50 @@
+import React, { useState } from 'react';
 
-import { ChevronLeft, ChevronRight } from '@untitled-ui/icons-react';
-import  {useState} from 'react';
+const Pagination = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const pages = [1, 2, 3, 4, 100];
 
-export default function PaginationControls({ totalPages, apiEndpoint, setResults}) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const pages = [];
-
-  const handlePageClick = (page) => {
-    if (page !== currentPage) {
-      setCurrentPage(page);
-    }
+  const handleClick = (index) => {
+    setCurrentIndex(index);
   };
 
-  const getPagination = () => {
+  const handlePrevious = () => {
+    setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev));
+  };
 
-    if (totalPages <= 7) {
-      for (let i = 1; i <= totalPages; i++) {
-        pages.push(i);
-      }
-    } 
-    else {
-      pages.push(1);
-
-      const start = Math.max(2, currentPage - 1);
-      const end = Math.min(totalPages - 1, currentPage + 1);
-
-      if (start > 2) pages.push('...');
-      for (let i = start; i <= end; i++) {
-        pages.push(i);
-      }
-      if (end < totalPages - 2) pages.push('...');
-      
-      pages.push(totalPages);
-    }
-
-    return pages;
-  }
-
-  const paginationItems = getPagination();
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev < pages.length - 1 ? prev + 1 : prev));
+  };
 
   return (
-    <>
-    {
-      totalPages > 1 &&
-      <div style={{ display: 'flex',gap:'24px', alignItems: 'center', alignSelf:'center' , margin:'20px'}} className='pagination-container'>
-        <button
-          className='pagination-btn'
-          onClick={() => currentPage > 1 && handlePageClick(currentPage - 1)}
-          disabled={currentPage === 1}
-          style={{
-            border: 'none',
-            cursor: currentPage === 1 ? 'not-allowed' : 'pointer',
-          }}
-        >
-          <ChevronLeft/>
-        </button>
-
-        {paginationItems.map((item, index) => (
-          <button
-            className={`pagination-btn ${item === currentPage ? 'active' : 'not-active'}`}
+    <div className="pagination">
+      <button
+        onClick={handlePrevious}
+        className={currentIndex === 0 ? '' : 'active'}
+        disabled={currentIndex === 0}
+      >
+        Prev.
+      </button>
+      <ul className='pagination-list'>
+        {pages.map((page, index) => (
+          <li
             key={index}
-            onClick={() => typeof item === 'number' && handlePageClick(item)}
-            disabled={item === '...'}
-            style={{
-              cursor: item === '...' ? 'default' : 'pointer',
-            }}
+            className={ `pagination-item ${index === currentIndex ? 'active' : ''}`}
+            onClick={() => handleClick(index)}
           >
-            {item}
-          </button>
+            {page}
+          </li>
         ))}
-
-        <button
-          className='pagination-btn'
-          onClick={() =>
-            currentPage < totalPages && handlePageClick(currentPage + 1)
-          }
-          disabled={currentPage === totalPages}
-          style={{
-            border: 'none',
-            cursor: currentPage === totalPages ? 'not-allowed' : 'pointer',
-          }}
-        >
-          <ChevronRight fontSize={'inherit'}/>
-        </button>
-      </div>
-    }
-    </>
+      </ul>
+      <button
+        onClick={handleNext}
+        className={currentIndex === pages.length - 1 ? '' : 'active'}
+        disabled={currentIndex === pages.length - 1}
+      >
+        Next
+      </button>
+    </div>
   );
-}
+};
+
+export default Pagination;
