dd7cfc311a5616aad886b8c0cb5b780a577e1f7d
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-08-18 18:49:20 +0200

    feat: added an updated backend for a dashboard

--- backend/api.js
+++ backend/api.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import { enhance } from '@zenstackhq/runtime';
+import { PrismaClient } from '@prisma/client';
+import { ZenStackMiddleware } from '@zenstackhq/server/express';
 import totalRoutes from './routes/TotalRoutes.js';
+import { RestApiHandler } from '@zenstackhq/server/api';
 
-
+const prisma = new PrismaClient();
 const REST_API = express.Router();
 
 REST_API.use(express.json());
 REST_API.use(cors());
+REST_API.use(
+    '/api/v2',
+    ZenStackMiddleware({
+        getPrisma: (request) => enhance(prisma),
+        handler: RestApiHandler({ endpoint: 'http://localhost:3000/api/v2' }),
+    })
+);
+
 REST_API.use(totalRoutes);
 
 export default REST_API;
