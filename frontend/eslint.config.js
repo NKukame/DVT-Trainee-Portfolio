d1bbd0e3b453a0a2d3c988dcf2edd387124325d1
Andile Mhlongo <amhlongo@dvtsoftware.com>
2025-08-27 00:08:36 +0200

    chore: updated eslint to ignore typescript files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,15 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { 
+    ignores: [
+      'dist',
+      'node_modules/**/*',
+      '**/*.ts',
+      '**/*.tsx',
+      '**/tsconfig.json'
+    ] 
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
