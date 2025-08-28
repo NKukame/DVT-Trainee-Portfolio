c10d83b5122b2c2e437fc1836d257b824e16b837
pthobei <pthobei@dvtsoftware.com>
2025-08-28 13:36:13 +0200

    updates: - Updating Employee Roles:    - added different types of developers, tester, product owner, etc.

---
+++ backend/prisma/migrations/20250828100644_added_new_employee_roles/migration.sql
@@ -0,0 +1,17 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'FULLSTACK_DEVELOPER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'FRONTEND_DEVELOPER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'BACKEND_DEVELOPER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'UX_UI_DESIGNER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'JUNIOR_DEVELOPER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'TESTER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'PRODUCT_OWNER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'SCRUM_MASTER';
+ALTER TYPE "public"."EmployeeRole" ADD VALUE 'DELIVERY_MANAGER';
