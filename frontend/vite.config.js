4293bfe533d7623db7400b085709777f335e9ad3
NKukame <NKukame@jhb.dvt.co.za>
2025-09-25 09:50:39 +0200

    Update Vite configuration to include rollupOptions for external dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['@mui/material/Grid2']
+    }
   },
   server: {
     historyApiFallback: true
